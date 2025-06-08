0799fbd86ccaab45d3c4c657f5f69b987ab6c9ca
azambrano43 <andres.a.zambrano.a@gmail.com>
2025-06-08 18:06:28 -0500

    Se modifica config/dyanodb.ts para que cargue variables de entorno.

--- src/config/dynamodb.ts
+++ src/config/dynamodb.ts
@@ -2,12 +2,16 @@
 
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import dotenv from 'dotenv';
 
 // 1. Configuración de la Región
 // El SDK buscará las credenciales (AWS_ACCESS_KEY_ID y AWS_SECRET_ACCESS_KEY)
 // en tus variables de entorno automáticamente.
 // Asegúrate de tener la región correcta donde crearás tus tablas.
-const REGION = process.env.AWS_REGION || "us-east-2";
+
+dotenv.config();
+const REGION = process.env.AWS_REGION || 'us-east-2';
+console.log(REGION);
 
 // 2. Creación del cliente base de DynamoDB
 const ddbClient = new DynamoDBClient({
